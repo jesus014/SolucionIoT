d31f1a8d5d0b032e8876d3dc8ec4149fea5495a1
SantiagoGuarneros <a17021089@iteshu.edu.mx>
2021-04-06 13:32:56 -0500

    Creacion del genericRepository cap04.

--- SolucionIoT.DAL/GenericRepository.cs
+++ SolucionIoT.DAL/GenericRepository.cs
@@ -1,10 +1,196 @@
-﻿using System;
+﻿using SolucionIoT.COMMON.Entidades;
+using SolucionIoT.COMMON.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using LiteDB;
+using FluentValidation;
 
 namespace SolucionIoT.DAL
 {
-    class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseDTO
     {
+        readonly string dbName = @"DBIoT.db";
+        public string Error { get;private set; }
+        readonly AbstractValidator<T> validator;
+
+        public IEnumerable<T> Read 
+        {
+            get
+            {
+                try
+                {
+                    IEnumerable<T> entidades;
+                    using (var db = new LiteDatabase(new ConnectionString() { Filename = dbName }))
+                    {
+                        entidades = db.GetCollection<T>(typeof(T).Name).FindAll();
+                    }
+                    Error = entidades != null ? "" : "No se encontraron la entidad";
+                    return entidades;
+                }
+                catch (Exception ex)
+                {
+
+                    Error = ex.Message;
+                    return null;
+                }
+            }
+        }
+
+        public GenericRepository(AbstractValidator<T> validator)
+        {
+            this.validator = validator;
+        }
+
+        public T Create(T entidad)
+        {
+            try
+            {
+                entidad.Id = Guid.NewGuid().ToString();
+                if (entidad.FechaHora == null)
+                {
+                    entidad.FechaHora = DateTime.Now;
+                }
+                var resultV = validator.Validate(entidad);
+                if (resultV.IsValid)
+                {
+                    using (var db = new LiteDatabase(new ConnectionString() { Filename = dbName }))
+                    {
+                        db.GetCollection<T>(typeof(T).Name).Insert(entidad);
+                    }
+                    Error = "";
+                    return entidad;
+                }
+                else
+                {
+                    Error = "Errores de validacion:";
+                    foreach (var item in resultV.Errors)
+                    {
+                        Error += item.ErrorMessage + ";";
+
+                    }
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Error = ex.Message;
+                return null;
+            }
+        }
+
+        public bool Delete(string id)
+        {
+            try
+            {
+                using (var db=new LiteDatabase(new ConnectionString() {Filename=dbName }))
+                {
+                    if (db.GetCollection<T>(typeof(T).Name).Delete(id)) 
+                    {
+                        Error = "";
+                        return true;
+                    }
+                    else
+                    {
+                        Error = "No se pudo eliminar la entidad";
+                        return false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Error = ex.Message;
+                return false;
+       
+            }       
+        }
+
+        public IEnumerable<T> Query(Expression<Func<T, bool>> predicado)
+        {
+            try
+            {
+               IEnumerable<T>  entidades;
+                using (var db = new LiteDatabase(new ConnectionString() { Filename = dbName }))
+                {
+                    entidades = db.GetCollection<T>(typeof(T).Name).Find(predicado);
+                }
+                Error = entidades != null ? "" : "No se encontraron la entidad";
+                return entidades;
+            }
+            catch (Exception ex)
+            {
+
+                Error = ex.Message;
+                return null;
+            }
+        }
+
+        public T SearchById(string id)
+        {
+            try
+            {
+                T entidad;
+                using (var db=new LiteDatabase(new ConnectionString() {Filename= dbName }))
+                {
+                    entidad = db.GetCollection<T>(typeof(T).Name).FindById(id);
+                }
+                Error = entidad != null ? "" : "No se encontro la entidad";
+                return entidad;
+            }
+            catch (Exception ex)
+            {
+
+                Error = ex.Message;
+                return null;
+            }
+        }
+
+        public T Update(T entidad)
+        {
+            try
+            {
+           
+                var resultV = validator.Validate(entidad);
+                if (resultV.IsValid)
+                {
+                    bool r;
+                    using (var db = new LiteDatabase(new ConnectionString() { Filename = dbName }))
+                    {
+                       r= db.GetCollection<T>(typeof(T).Name).Delete(entidad.Id);
+                    }
+                    if (r)
+                    {
+                        Error = "";
+                        return entidad;
+                    }
+                    else
+                    {
+                        Error = "No se actualizo la entidad";
+                        return null;
+
+                    }
+              
+                }
+                else
+                {
+                    Error = "Errores de validacion:";
+                    foreach (var item in resultV.Errors)
+                    {
+                        Error += item.ErrorMessage + ";";
+
+                    }
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Error = ex.Message;
+                return null;
+            }
+        }
     }
 }
