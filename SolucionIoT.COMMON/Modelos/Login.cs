b9d5a30ec3710fd439be5584ed55682d3bd41c81
SantiagoGuarneros <a17021089@iteshu.edu.mx>
2021-04-11 14:19:07 -0500

    Creacion de plataforma web para proyecto

---
+++ SolucionIoT.COMMON/Modelos/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolucionIoT.COMMON.Modelos
+{
+    public class Login
+    {
+        public string  Correo { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SolucionIoT.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using SolucionIoT.COMMON.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolucionIoT.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public Login Login { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
